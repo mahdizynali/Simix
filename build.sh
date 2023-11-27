12a60a97bba3da7ed6e185f5f6950cb26a4aac18
Mahdi Zeinali <zeinali.mahdy@gmail.com>
2023-11-27 15:24:02 +0330

    update packges

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 sudo rm -f setup/boot/initramfs.cpio.gz simix.iso
-sudo apt install build-essential grub-pc qemu-kvm -y
+sudo apt install build-essential grub-pc qemu-kvm xorriso -y
 cd rootfs/ && find . | cpio -o -H newc | gzip > ../setup/boot/initramfs.cpio.gz && cd ..
 grub-mkrescue -o simix.iso ./setup
 qemu-system-x86_64 -cdrom simix.iso
