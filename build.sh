19e6e62739585a72735a3c41d28376a77c9901cc
Mahdi Zeinali <zeinali.mahdy@gmail.com>
2023-11-27 11:53:45 +0330

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 sudo apt update
 sudo apt install build-essential grub-pc qemu-system-x86_64
-cd root/ && find . | cpio -o -H newc | gzip > ../setup/boot/initramfs.cpio.gz && cd ..
+cd rootfs/ && find . | cpio -o -H newc | gzip > ../setup/boot/initramfs.cpio.gz && cd ..
 grub-mkrescue -o simix.iso ./setup
 qemu-system-x86_64 -cdrom simix.iso
