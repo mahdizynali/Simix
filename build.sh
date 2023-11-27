831cb192accc98f69170c4b2d19208820b511008
mahdizynali <zeinali.mahdy@gmail.com>
2023-11-27 12:25:26 +0330

    remove last config

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+sudo rm -f setup/boot/initramfs.cpio.gz simix.iso
 sudo apt install build-essential grub-pc qemu-kvm -y
 cd rootfs/ && find . | cpio -o -H newc | gzip > ../setup/boot/initramfs.cpio.gz && cd ..
 grub-mkrescue -o simix.iso ./setup
