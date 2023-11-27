9cbd2466823b0a1e9a8d93a044818cba0a93ca8b
mahdizynali <zeinali.mahdy@gmail.com>
2023-11-27 12:12:15 +0330

    update config

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
-sudo apt update
-sudo apt install build-essential grub-pc qemu-system-x86_64
+sudo apt install build-essential grub-pc qemu-kvm -y
 cd rootfs/ && find . | cpio -o -H newc | gzip > ../setup/boot/initramfs.cpio.gz && cd ..
 grub-mkrescue -o simix.iso ./setup
 qemu-system-x86_64 -cdrom simix.iso
