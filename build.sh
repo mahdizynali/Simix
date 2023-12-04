01c5058a806802021808aed804a33e219460c132
mahdizynali <zeinali.mahdy@gmail.com>
2023-12-04 11:55:39 +0330

    update builder config

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 sudo rm -f setup/boot/initramfs.cpio.gz simix.iso
 sudo apt install build-essential grub-pc qemu-kvm xorriso mtools -y
-cd rootfs/ && find . | cpio -o -H newc | gzip > ../setup/boot/initramfs.cpio.gz && cd ..
+
+if [ -d "rootfs" ]; then
+    cd rootfs/ && mkdir -p {home,lib,lib64,proc,root,run,sys,tmp,var/cache,var/log,var/run,var/tmp,usr/bin,usr/sbin}
+    find . | cpio -o -H newc | gzip > ../setup/boot/initramfs.cpio.gz && cd ..
+else
+    echo "rootfs file not found !!"
+fi
 grub-mkrescue -o simix.iso ./setup
 qemu-system-x86_64 -cdrom simix.iso
